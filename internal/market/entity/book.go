d25eb45bf87aaf00b2e9800a25c7fc7d4ab29ced
Alisson Santana <alissonsantana16608@gmail.com>
2023-12-05 08:19:18 -0300

    iniciando no go

---
+++ internal/market/entity/book.go
@@ -0,0 +1,11 @@
+package entity
+
+import "sync"
+
+type Book struct {
+	Order        []*Order
+	Transaction  []*Transaction
+	OrderChan    chan *Order
+	OrderChanOut chan *Order
+	Wg           *sync.WaitGroup
+}
